caeabae868af0e8e1741c313f077a52adaf4eaa8
Ahmed Amin <ahmed.amin@wexcute.com>
2023-12-07 13:27:01 +0100

    LEAR-52 add create List Loginc

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,46 @@
+// sections
+const secTasksLists = document.getElementById("tasksLists")
+
+
+// divs
 const divAddList = document.getElementById("IdAddListCon")
+const divdummy = document.getElementById("dummy")
+
+
+// btns
 const btnAddList = document.getElementById("btnShowAddList")
+const btnCreateList = document.getElementById("btnCreateList")
+const btnHideAddList = document.getElementById("btnHideAddList")
+
+// txt
+const txtListName = document.getElementById("txtListName")
+
+
+
+// event lisners
 btnAddList.addEventListener("click", showAddList);
+btnHideAddList.addEventListener("click", hideAddList);
+btnCreateList.addEventListener("click", createList);
+
+
+// functions
 function showAddList() {
     divAddList.classList.remove("hidden")
     btnAddList.classList.add("hidden")
+}
+
+function hideAddList() {
+    divAddList.classList.add("hidden")
+    btnAddList.classList.remove("hidden")
+}
+function createList() {
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = divdummy.innerHTML;
+
+
+    newDiv.classList.remove("hidden");
+    newDiv.classList.add("cardCoontainer");
+    
+    divAddList.parentNode.parentNode.insertBefore(newDiv,divAddList.parentNode);
+
 }
