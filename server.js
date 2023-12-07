2f386d96b8e1500e7d16dff8b907420431d18201
Ahmed Amin <ahmed.amin@wexcute.com>
2023-12-07 14:56:34 +0100

    LEAR-52 add api logic in server.js

--- server.js
+++ server.js
@@ -5,9 +5,62 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/')));
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../index.html'));
-// });
+app.use(express.json()); 
+var id =1
+const tasks = [
+  { id: id, listName: 'container1', taskName: 'task1' },
+  // Add more tasks as needed
+];
+
+// CRUD operations
+app.get('/api/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+
+app.get('/api/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const index = tasks.findIndex(task => task.id === taskId);
+
+  if (index !== -1) {
+    res.json(tasks.find(task => task.id === taskId));
+  } else {
+    res.status(404).json({ error: 'Task not found' });
+  }
+});
+
+app.post('/api/tasks', (req, res) => {
+  const newTask = req.body;
+  id++
+  newTask.id = id
+  tasks.push(newTask);
+  res.json(newTask);
+});
+
+app.put('/api/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const updatedTask = req.body;
+  const index = tasks.findIndex(task => task.id === taskId);
+
+  if (index !== -1) {
+    tasks[index] = { ...tasks[index], ...updatedTask };
+    res.json(tasks[index]);
+  } else {
+    res.status(404).json({ error: 'Task not found' });
+  }
+});
+
+app.delete('/api/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const index = tasks.findIndex(task => task.id === taskId);
+
+  if (index !== -1) {
+    const deletedTask = tasks.splice(index, 1);
+    res.json(deletedTask[0]);
+  } else {
+    res.status(404).json({ error: 'Task not found' });
+  }
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
